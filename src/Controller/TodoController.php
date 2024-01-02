f56d75ed15286465db8da3c4e9fcef081a2e2037
aymensellaouti <aymen.sellaouti@gmail.com>
2024-01-02 17:08:16 +0100

    add Todo

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -22,11 +22,36 @@ class TodoController extends AbstractController
             ];
             $session->set('todos', $todos);
             $this->addFlash('infos', "Vous venez d'initialiser un tableau de todo");
-            $this->addFlash('infos', "Je vous assure, vous venez d'initialiser un tableau de todo");
+//            $this->addFlash('infos', "Je vous assure, vous venez d'initialiser un tableau de todo");
         }
 
         return $this->render('todo/index.html.twig', [
             'controller_name' => 'TodoController',
         ]);
+    }
+
+    #[Route("todo/add/{name}/{content}", name:"app_todo_add")]
+    public function addTodo(SessionInterface $session, $name, $content) {
+        // 1- Vérifier que la session contient déjà les todos
+        if (!$session->has('todos')) {
+            // 1-1 Sinon
+            // Ajouter un message flash pour informer que la liste n'est pas encore initialisée
+            $this->addFlash('error', "La session n'est pas encore initialisée");
+        } else {
+            // 1-2 Si oui
+            // 1-2-1 Vérifier l'existance du todo
+            $todos = $session->get('todos');
+            if (isset($todos[$name])) {
+                //1-2-1-1 si oui flash erreur
+                $this->addFlash('error', "Le todo de clé $name existe déjà");
+            } else {
+                //1-2-1-2 si non on l'ajoute + flash succèes
+                $todos[$name] = $content;
+                $session->set('todos', $todos);
+                $this->addFlash('success', "Le todo de clé $name a été ajouté avec succèes");
+            }
+        }
+        //On réaffiche la liste des todos
+        return $this->redirectToRoute('app_todo');
     }
 }
